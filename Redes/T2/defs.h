02663b0c8c38b6895af66266d46ab6689a76b380
Éverton <eassis.vieira@gmail.com>
2018-12-02 20:54:41 -0200

    *.*

---
+++ Redes/T2/defs.h
@@ -0,0 +1,16 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define INF (1 << 7)
+
+#define MAX_ROUTERS_QT 32
+#define MSG_SIZE 256
+#define PKG_QUEUE_SIZE 32
+#define TIMER_QUEUE_SIZE 64
+#define VECTOR_SEND_PERIOD 3
+
+#define MSG 0
+#define CONTROL 1
+#define ACK 2
+
+#endif
