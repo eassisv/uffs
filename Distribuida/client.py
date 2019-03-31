4dd7d7c9972ddd0f319572b8cbadd1796ecb4679
Éverton Vieira <eassis.vieira@gmail.com>
2019-03-31 19:56:14 -0300

    Uhuu

--- Distribuida/client.py
+++ Distribuida/client.py
@@ -1,21 +1,50 @@
+# -*- coding: utf-8 -*-
+
 import socket
+from threading import Thread
+from time import sleep
 
-host = '127.0.0.1'
-port = 25010
-
-sock = socket.socket()
-
-while True:
-    message = input('Mensagem: ')
-    if message == 'quit':
-        break
-
-    sock.connect((host, port))
-    sock.send(message.encode())
-
-    resp = sock.recv(512).decode()
-    sock.shutdown(socket.SHUT_RDWR)
-
-    print(resp)
+address = '127.0.0.1'
+port = 25122
 
 
+def sender():
+    sock = socket.socket()
+    sock.settimeout(5)
+    while True:
+        msg = input('--> ')
+        if msg == 'quit':
+            break
+        try:
+            sock.connect((address, port))
+        except (socket.timeout, ConnectionRefusedError):
+            print('Impossível conectar')
+            continue
+        sock.send(msg.encode())
+        data = sock.recv(1024)
+        print('sender: {}'.format(data.decode()))
+
+
+def receiver(running):
+    sock = socket.socket()
+    sock.settimeout(5)
+    sock.bind((address, port))
+    sock.listen(10)
+    while running:
+        try:
+            conn, addr = sock.accept()
+        except socket.timeout:
+            sock.close()
+            continue
+        data = conn.recv(1024)
+        print('receiver: {}'.format(data.decode()))
+        conn.send(data)
+
+
+if __name__ == '__main__':
+    running = True
+    send = Thread(target=sender, name='sender')
+
+    send.start()
+
+    send.join()
--- Distribuida/main.py
+++ Distribuida/main.py
@@ -5,50 +5,52 @@ from threading import Thread
 from time import sleep
 
 address = '127.0.0.1'
-port = 25122    
+port = 25001
+running = True
 
 
 def sender():
     while True:
-        sock = socket.socket()
-        sock.settimeout(5)
         msg = input('--> ')
         if msg == 'quit':
+            print('Saindo...')
             break
+        sock = socket.socket()
+        sock.settimeout(5)
         try:
             sock.connect((address, port))
-        except (socket.timeout, ConnectionRefusedError):
-            print('Impossível conectar')
+        except socket.timeout:
+            print('Impossível connectar')
             continue
+            sock.close()
         sock.send(msg.encode())
-        data = sock.recv(1024)
-        print('sender: {}'.format(data.decode()))
+        response = sock.recv(1024)
+        print('response: ', response.decode())
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
 
 
-def receiver(running):
+def receiver():
+    sock = socket.socket()
+    sock.settimeout(5)
+    sock.bind((address, port))
+    sock.listen(10)
     while running:
-        sock = socket.socket()
-        sock.settimeout(5)
-        sock.bind((address, port))
-        sock.listen(10)
         try:
             conn, addr = sock.accept()
         except socket.timeout:
-            sock.close()
             continue
         data = conn.recv(1024)
-        print('receiver: {}'.format(data.decode()))
+        print('receiver: ', data.decode())
         conn.send(data)
-        sock.shutdown(socket.SHUT_RDWR)
-        sock.close()
-        sleep(10)
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
+    sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
 
 
 if __name__ == '__main__':
-    running = True
-    rec = Thread(target=receiver, name='receiver', args=(running,))
+    rec = Thread(target=receiver, name='receiver')
     send = Thread(target=sender, name='sender')
 
     rec.start()
