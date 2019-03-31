8952bba933a2a385b9744f4db15d72224ebeaf64
Éverton Vieira <eassis.vieira@gmail.com>
2019-03-31 18:27:27 -0300

    Hue

--- Distribuida/main.py
+++ Distribuida/main.py
@@ -5,7 +5,7 @@ from threading import Thread
 from time import sleep
 
 address = '127.0.0.1'
-port = 25111
+port = 25122    
 
 
 def sender():
@@ -30,19 +30,20 @@ def sender():
 def receiver(running):
     while running:
         sock = socket.socket()
-        sock.close()
         sock.settimeout(5)
         sock.bind((address, port))
         sock.listen(10)
         try:
             conn, addr = sock.accept()
         except socket.timeout:
+            sock.close()
             continue
         data = conn.recv(1024)
         print('receiver: {}'.format(data.decode()))
         conn.send(data)
         sock.shutdown(socket.SHUT_RDWR)
         sock.close()
+        sleep(10)
 
 
 if __name__ == '__main__':
