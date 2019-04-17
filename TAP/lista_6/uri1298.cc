21730380c0ce22ad7aa18bcefb2b1243262cc378
Éverton Vieira <eassis.vieira@gmail.com>
2019-04-17 17:07:17 -0300

    problema uri1298 em andamento, lista de TAP

--- TAP/lista_6/uri1298.cc
+++ TAP/lista_6/uri1298.cc
@@ -1,28 +1,56 @@
 #include <utility>
 #include <queue>
+#include <string>
+#include <vector>
 #include <cstdio>
 #include <cstring>
 using namespace std;
 
 #define MAX 3123
+#define valid(x, y) (x > 0 && x < 2 * n -1) \
+                    
+
 typedef pair<int, int> ii;
 
-char maze[MAX][MAX];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
 bool seen[2 * MAX][2 * MAX];
+int n;
 
-int dfs(int i, int j) {
-
+int bfs(int i, int j, vector<string> &maze) {
+    int x, y, ans = 1;
+    queue<ii> Q;
+    Q.push(make_pair(i, j));
+    while (!Q.empty()) {
+        x = Q.front().first; y = Q.front().second;
+        Q.pop();
+        seen[x][y] = true;
+        for (int k = 0; k < 4; k++) {
+            i = x + dx[k]; j = y + dy[k];
+            if (valid(x, y)) {
+               if (x == 2 * n - 1 && !y) ans = 0; 
+            }
+        }
+    }
+    return ans;
 }
 
 int main(void) {
-    int n, ans;
+    int ans;
+    char line[2 * MAX];
     while (scanf("%d", &n) != EOF) {
-        for (int i = 0; i < 2 * n; i++)
-            scanf(" %s", (char *)(maze + i));
+        vector<string> maze;
+        memset(seen, 0, sizeof(seen));
+        for (int i = 0; i < 2 * n - 1; i++) {
+            scanf(" %s", line);
+            if (i & 1) maze.push_back(string(" ") + string(line)); 
+            else maze.push_back(string(line) + string(" "));
+            maze.push_back(maze.back());
+        }
         ans = 0;
         for (int i = 0; i < 2 * n; i++)
             for (int j = 0; j <= 2 * n; j++)
-                ans += dfs(i, j);
+                ans += bfs(i, j, maze);
         printf("%d\n", ans);
     }
     return 0;
