081362c79928bcc3785a06fde5eb6adf6dac7736
Éverton Vieira <eassis.vieira@gmail.com>
2019-05-14 00:31:29 -0300

    Finalizado trabalho de CD

--- computacao-distribuida/byzantine-generals/main.py
+++ computacao-distribuida/byzantine-generals/main.py
@@ -3,6 +3,7 @@
 import sys
 import socket
 import re
+import random
 from pickle import loads, dumps
 from threading import Thread
 from string import ascii_lowercase
@@ -18,19 +19,19 @@ neighbors = set()
 
 def broadcast(msg):
     ng = []
+    global my_id
+    global faulty
     for i in range(1, len(configs)):
         sock = socket.socket()
         sock.settimeout(5)
-        if faulty:
-            msg = ascii_lowercase[i]
+        if faulty and (type(msg[my_id]) is str):
+            msg = {my_id: random.choice(ascii_lowercase)}
         try:
             sock.connect(configs[i])
             sock.send(dumps(msg))
         except (socket.timeout, ConnectionRefusedError):
-            print('Impossível conectar com {}'.format(i + 1))
+            # print('Impossível conectar com {}'.format(i + 1))
             continue
-        except InterruptedError:
-            print('Deu merda')
         sock.close()
         ng.append(i + 1)
     global neighbors
@@ -38,6 +39,7 @@ def broadcast(msg):
 
 
 def sender_general():
+    global my_id
     while True:
         msg = input('-> ')
         if msg == 'sair':
@@ -49,13 +51,15 @@ def sender_general():
 
 
 def receiver(my_config):
+    global neighbors
+    global my_id
+    global vector
     sock = socket.socket()
     sock.bind(my_config)
     sock.settimeout(5)
     sock.listen(10)
     vector[my_id] = {}
     ng = set()
-    global neighbors
     while running:
         try:
             conn, _ = sock.accept()
@@ -63,7 +67,6 @@ def receiver(my_config):
             continue
         data = loads(conn.recv(1024))
         v = data.copy().popitem()
-        print('data depois do bagulho', data)
         if v[0] == 1:
             # Recebendo do general
             vector[my_id] = data
@@ -73,24 +76,33 @@ def receiver(my_config):
             vector[my_id].update(data)
             if ng == neighbors:
                 ng = set()
-                print('Mandando ver')
                 broadcast(vector)
         elif type(v[1]) is dict:
-            print("ABACAXI")
-            print('Recebi vetor de {} = {}'.format(v[0], v[1]))
             # Recebendo os vetores
             vector.update(data)
             ng |= set(list(data.keys()))
             if ng == neighbors:
-                # ver qual é o faulty
-                print('Acabou')
-                print("-"*30)
-                print(vector)
-                break
-
-        print(vector)
-        print(ng)
-        print(neighbors)
+                count = {}
+                for key in vector.keys():
+                    for value in vector[key].values():
+                        if value in count:
+                            count[value] += 1
+                        else:
+                            count[value] = 1
+                count = sorted(
+                    count.items(), key=lambda x: (x[1]), reverse=True)
+                faulty_list = []
+                for f in count[1::]:
+                    for key in vector.keys():
+                        for _key, value in vector[key].items():
+                            if value == f[0]:
+                                faulty_list.append(_key)
+                print('Lista de defeituosos')
+                for f in set(faulty_list):
+                    print(f)
+                vector = {}
+                neighbors = set()
+                ng = set()
 
         conn.shutdown(socket.SHUT_RDWR)
         conn.close()
@@ -130,7 +142,6 @@ if __name__ == '__main__':
     try:
         my_id = int(sys.argv[1])
         neighbors |= set([my_id])
-        print(neighbors)
         # Se o id do processo é 1 ele é o general
         general = True if my_id == 1 else False
         # Se o segundo argumento for 'faulty' o processo é o mentiroso
@@ -141,10 +152,9 @@ if __name__ == '__main__':
         print('Execute o programa com o padrão {} <id>'.
               format(sys.argv[0]))
         sys.exit(0)
-
-    print('Digite "sair" para encerrar')
-
+    if my_id == 1:
+        print('Digite "sair" para encerrar')
+    else:
+        print('Aperte ^C para encerrar')
     my_config = read_configs()
-    print(configs)
-    print(my_config)
     start_threads()
