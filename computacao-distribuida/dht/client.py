83c06a8fb8a34915b4163fffa560ff4ce3531eff
Éverton Vieira <eassis.vieira@gmail.com>
2019-05-26 18:54:24 -0300

    dht CD

---
+++ computacao-distribuida/dht/client.py
@@ -0,0 +1,42 @@
+import socket
+from pickle import loads, dumps
+
+
+if __name__ == "__main__":
+    servers_port = [25001, 25002, 25003, 25004]
+    while True:
+        print("1 - buscar dado\n2 - inserir dado\n3 - remover dado\n0 - sair")
+        try:
+            op = int(input("-> "))
+            if op < 0 or op > 3:
+                raise ValueError
+        except ValueError:
+            print("Opção inválida")
+            continue
+        if op == 0:
+            print("Saindo...")
+            break
+        print("Digite o dado: ", end="")
+        data = input()
+        _hash = abs(hash(data))
+        msg = {"op": op, "hash": _hash, "data": data}
+        port = servers_port[_hash % 4]
+        sock = socket.socket()
+        sock.connect(("127.0.0.1", port))
+        sock.send(dumps(msg))
+        data = loads(sock.recv(1024))
+        if op == 1:
+            if data["found"]:
+                print("Dado encontrado no servidor {}".format(data["id"]))
+            else:
+                print("Dado não encontrado")
+        elif op == 2:
+            if data["inserted"]:
+                print("Dado inserido no servidor {}".format(data["id"]))
+            else:
+                print("O dado não foi inserido")
+        elif op == 3:
+            if data["removed"]:
+                print("Dado removido do servidor {}".format(data["id"]))
+            else:
+                print("Dado não encontrado para remoção")
---
+++ computacao-distribuida/dht/servers.py
@@ -0,0 +1,102 @@
+import socket, sys
+from threading import Thread
+from pickle import loads, dumps
+
+running = False
+
+
+def insertion(data, hash_table):
+    _hash = abs(hash(data["hash"]))
+    key = _hash % len(hash_table)
+    done = False
+    for i in range(0, len(hash_table) - 1):
+        if hash_table[(key + i) % len(hash_table)] == None:
+            hash_table[(key + i) % len(hash_table)] = (_hash, data)
+            done = True
+            break
+    if done:
+        ans = {"inserted": True, "hash": data["hash"]}
+    else:
+        ans = {"inserted": False}
+    return ans
+
+
+def search(data, hash_table):
+    _hash = abs(hash(data["hash"]))
+    key = _hash % len(hash_table)
+    found = False
+    for i in range(0, len(hash_table) - 1):
+        if hash_table[(key + i) % len(hash_table)] == None:
+            continue
+        if (
+            hash_table[(key + i) % len(hash_table)][1]["hash"] == data["hash"]
+            and hash_table[(key + i) % len(hash_table)][0] == _hash
+        ):
+            ans = {"found": True, "data": hash_table[(key + i) % len(hash_table)][1]}
+            found = True
+            break
+    if not found:
+        ans = {"found": False}
+    return ans
+
+
+def remove(data, hash_table):
+    _hash = abs(hash(data["hash"]))
+    key = _hash % len(hash_table)
+    removed = False
+    for i in range(0, len(hash_table) - 1):
+        if hash_table[(key + i) % len(hash_table)] == None:
+            continue
+        if (
+            hash_table[(key + i) % len(hash_table)][1]["hash"] == data["hash"]
+            and hash_table[(key + i) % len(hash_table)][0] == _hash
+        ):
+            hash_table[key + i] = None
+            removed = True
+            break
+    if removed:
+        ans = {"removed": True}
+    else:
+        ans = {"removed": False}
+    return ans
+
+
+def server(_id, port):
+    hash_table = [None] * 8
+    sock = socket.socket()
+    sock.bind(("127.0.0.1", port))
+    sock.settimeout(5)
+    sock.listen(10)
+    while running:
+        try:
+            conn, _ = sock.accept()
+        except socket.timeout:
+            continue
+        data = loads(conn.recv(1024))
+        print(data)
+        if data["op"] == 1:
+            ans = search(data, hash_table)
+        elif data["op"] == 2:
+            ans = insertion(data, hash_table)
+        else:
+            ans = remove(data, hash_table)
+        ans["id"] = _id
+        conn.send(dumps(ans))
+        conn.shutdown(socket.SHUT_RDWR)
+        conn.close()
+
+
+if __name__ == "__main__":
+    ports = [25001, 25002, 25003, 25004]
+    addr = "127.0.0.1"
+    servers = []
+    running = True
+    for _id, port in zip(range(0, 4), ports):
+        servers.append(Thread(target=server, name=f"server_{port}", args=(_id, port)))
+        servers[-1].start()
+    try:
+        for s in servers:
+            s.join()
+    except KeyboardInterrupt:
+        print("\r\rSaindo...")
+        running = False
