df49f0da7c7153fa8b4fc2eb936f44aa3de847b8
Éverton de Assis <eassis.vieira@gmail.com>
2019-06-19 20:16:35 -0300

    adicionada solução para o problema 1127 da lista 12 de tap

--- TAP/lista_12/uri1127.cc
+++ TAP/lista_12/uri1127.cc
@@ -3,19 +3,48 @@
 #include <string>
 using namespace std;
 
-#define MAX 112345
+#define MAX 212345
+#define cmp compare
 
-int m, t, ms[MAX], ts[MAX];
+int m, t, ms[MAX], ts[MAX], b[MAX];
 
 string notes[] = {
   "A", "A#","B", "C", "C#", "D",
   "D#", "E", "F", "F#", "G", "G#"
 };
 
+string notes2[] = {
+  "", "Bb", "Cb", "B#", "Db", "",
+  "Eb", "Fb", "E#", "Gb", "", "Ab" 
+};
+
 int val(string note) {
   for (int i = 0; i < 12; i++)
-    if (!notes[i].compare(note))
+    if (!notes[i].cmp(note))
       return i;
+  for (int i = 0; i < 12; i++)
+    if (!notes2[i].cmp(note))
+      return i;
+  return 0;
+}
+
+void kmpre() {
+  int i = 0, j = -1; b[0] = -1;
+  while (i < t) {
+    while (j >= 0 && ts[i] != ts[j]) j = b[j];
+    i++; j++;
+    b[i] = j;
+  }
+}
+
+int kmp() {
+  int i = 0, j = 0;
+  while (i < m) {
+    while (j >= 0 && ms[i] != ts[j]) j = b[j];
+    i++; j++;
+    if (j == t) return 1;
+  }
+  return 0;
 }
 
 int main(void) {
@@ -27,17 +56,18 @@ int main(void) {
       int *w = j ? ts: ms; 
       for (int i = 0; i < s; i++) {
         scanf(" %s", note);
-        if (note[1] == 'b') {
-          note[1] = '#';
-          note[0] = note[0] == 'A' ? 'G' : note[0] - 1;
-        }
         v = val(string{note});
-        if (i) w[i - 1] = v - p;
+        if (i) {
+          w[i - 1] = (v - p + 12) % 12;
+        }
         p = v;
       }
     }
-    for (int i = 0; i < m - 1; i++) printf("%d ", ms[i]); puts("");
-    for (int i = 0; i < t - 1; i++) printf("%d ", ts[i]); puts("");
+    m--; t--;
+    // for (int i = 0; i < m; i++) printf("%d ", ms[i]); puts("");
+    // for (int i = 0; i < t; i++) printf("%d ", ts[i]); puts("");
+    kmpre();
+    printf("%c\n", kmp() ? 'S' : 'N');
   } 
   return 0;
 }
